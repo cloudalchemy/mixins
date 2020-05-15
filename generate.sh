2cc1fb102cd279eda3b464e1f79e8116aabf3c08
paulfantom <pawel@krupa.net.pl>
2020-05-15 14:45:44 +0200

    list dashboards

--- generate.sh
+++ generate.sh
@@ -10,6 +10,7 @@ download_mixin() {
 	local mixin="$1"
 	local repo="$2"
 	local subdir="$3"
+	local curdir=$(pwd)
 	
 	cd "$TMPDIR"
 	git clone --depth 1 --filter=blob:none "$repo" "$mixin"
@@ -22,63 +23,72 @@ download_mixin() {
 	jsonnet -J vendor -S -e 'std.manifestYamlDoc((import "mixin.libsonnet").prometheusAlerts)' | gojsontoyaml > $TOP/$MANIFESTS/$mixin/alerts.yaml || :
 	jsonnet -J vendor -S -e 'std.manifestYamlDoc((import "mixin.libsonnet").prometheusRules)' | gojsontoyaml > $TOP/$MANIFESTS/$mixin/rules.yaml || :
 	jsonnet -J vendor -m $TOP/$MANIFESTS/$mixin/dashboards -e '(import "mixin.libsonnet").grafanaDashboards' || :
+	cd "$curdir"
 }
 
 # Prepare env
-rm -rf $MANIFESTS $TMPDIR
+rm -rf site/content/* $MANIFESTS $TMPDIR
 mkdir -p $TMPDIR
-
-# Generate manifests
-cat mixins.yaml | gojsontoyaml -yamltojson > $TMPDIR/mixins.json
-
-for mixin in $(cat $TMPDIR/mixins.json | jq -r '.mixins[].name'); do
-	repo="$(cat $TMPDIR/mixins.json | jq -r ".mixins[] | select(.name == \"$mixin\") | .source")"
-	subdir="$(cat $TMPDIR/mixins.json | jq -r ".mixins[] | select(.name == \"$mixin\") | .subdir")"
-	set +u
-	download_mixin $mixin $repo $subdir
-	#set -u
-done
-
-# Remove previously generated site
-cd $TOP
-rm -rf site/content/*
 
 # Create top-level index.md header
 INDEXFILE="site/content/_index.md"
 touch $INDEXFILE
 cat <<EOF > $INDEXFILE
 ---
-title: Generated monitoring mixins
+title: Prometheus monitoring mixins
 ---
 
 EOF
 
-# Create index.md files for each mixin and add it to global index.md
-for dir in ${MANIFESTS}/*; do
-	mixin=$(basename $dir)
+# Generate manifests
+cat mixins.yaml | gojsontoyaml -yamltojson > $TMPDIR/mixins.json
+
+for mixin in $(cat $TMPDIR/mixins.json | jq -r '.mixins[].name'); do
+	cd $TOP
+	repo="$(cat $TMPDIR/mixins.json | jq -r ".mixins[] | select(.name == \"$mixin\") | .source")"
+	subdir="$(cat $TMPDIR/mixins.json | jq -r ".mixins[] | select(.name == \"$mixin\") | .subdir")"
+	text="$(cat $TMPDIR/mixins.json | jq -r ".mixins[] | select(.name == \"$mixin\") | .text")"
+	if [ "$text" == "null" ]; then text=""; fi
+	set +u
+	download_mixin $mixin $repo $subdir
+	#set -u
+
 	mkdir -p "site/content/${mixin}"
 	file="site/content/${mixin}/index.md"
 	# Create header
+	if [ -n "$subdir" ]; then
+		location="$repo/tree/master/$subdir"
+	else
+		location="$repo"
+	fi
 	cat << EOF > $file
 ---
 title: $mixin
 ---
 
-EOF
-	name=${mixin%.*}
-	echo -e "\n## $name\n" >> $INDEXFILE
+$text
 
+Mixin available at [${repo#*//}]($location)
+
+EOF
+	echo -e "\n## $mixin\n" >> $INDEXFILE
+	
+	dir="$TOP/$MANIFESTS/$mixin"
 	if [ -s "$dir/alerts.yaml" ] && [ "$(stat -c%s "$dir/alerts.yaml")" -gt 20 ]; then
 		echo -e "# Alerts\n\n[embedmd]:# (../../../$MANIFESTS/$mixin/alerts.yaml yaml)\n" >> $file
-		echo "- [Alerts](/$name#alerts)" >> $INDEXFILE
+		echo "- [Alerts](/$mixin#alerts)" >> $INDEXFILE
 	fi
 	if [ -s "$dir/rules.yaml" ] && [ "$(stat -c%s "$dir/rules.yaml")" -gt 20 ]; then
 		echo -e "# Recording rules\n\n[embedmd]:# (../../../$MANIFESTS/$mixin/rules.yaml yaml)\n" >> $file
-		echo "- [Recording Rules](/$name#rules)" >> $INDEXFILE
+		echo "- [Recording Rules](/$mixin#recording-rules)" >> $INDEXFILE
 	fi
-	if [ -s "$dir/dashboards.yaml" ]; then
-		echo -e "# Dashboards\n\n[embedmd]:# (../../../$MANIFESTS/$mixin/dashboards.yaml yaml)\n" >> $file
-		echo "- [Dashboards](/$name#dashboards)" >> $INDEXFILE
+	if [ "$(ls -A "$dir/dashboards")" ]; then
+		echo -e "# Dashboards\nFollowing dashboards are generated from mixins and hosted on github:\n\n" >> $file
+		for dashboard in $dir/dashboards/*.json; do
+			d=$(basename $dashboard)
+			echo "- [${d%.*}](https://github.com/cloudalchemy/mixins/blob/master/manifests/$mixin/dashboards/$d)" >> $file
+		done
+		echo "- [Dashboards](/$mixin#dashboards)" >> $INDEXFILE
 	fi
 done
 
