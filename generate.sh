941d5fc3e97f176d7215197881ae2c1297d5e2ef
paulfantom <pawel@krupa.net.pl>
2020-05-20 12:51:34 +0200

    simplify generator script

--- generate.sh
+++ generate.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-
+#shellcheck disable=SC2129,SC2164
 #set -euo pipefail
 
-TOP=$(pwd)
-TMPDIR="$(pwd)/tmp"
-MANIFESTS="manifests"
+MANIFESTS="assets"
+TOP=$(git rev-parse --show-toplevel)
+TMPDIR="${TOP}/tmp"
 
 download_mixin() {
 	local mixin="$1"
 	local repo="$2"
 	local subdir="$3"
-	local curdir=$(pwd)
+
+	git clone --depth 1 --filter=blob:none "$repo" "${TMPDIR}/$mixin"
+	mkdir -p "${TOP}/${MANIFESTS}/${mixin}/dashboards"
+	(
+		cd "${TMPDIR}/${mixin}/${subdir}"
+		if [ -f "jsonnetfile.json" ]; then
+			jb install
+		fi
 	
-	cd "$TMPDIR"
-	git clone --depth 1 --filter=blob:none "$repo" "$mixin"
-	cd "$mixin/$subdir"
-	if [ -f "jsonnetfile.json" ]; then
-		jb install
-	fi
-	
-	mkdir -p $TOP/$MANIFESTS/$mixin/dashboards
-	jsonnet -J vendor -S -e 'std.manifestYamlDoc((import "mixin.libsonnet").prometheusAlerts)' | gojsontoyaml > $TOP/$MANIFESTS/$mixin/alerts.yaml || :
-	jsonnet -J vendor -S -e 'std.manifestYamlDoc((import "mixin.libsonnet").prometheusRules)' | gojsontoyaml > $TOP/$MANIFESTS/$mixin/rules.yaml || :
-	jsonnet -J vendor -m $TOP/$MANIFESTS/$mixin/dashboards -e '(import "mixin.libsonnet").grafanaDashboards' || :
-	cd "$curdir"
+		jsonnet -J vendor -S -e 'std.manifestYamlDoc((import "mixin.libsonnet").prometheusAlerts)' | gojsontoyaml > "${TOP}/${MANIFESTS}/${mixin}/alerts.yaml" || :
+		jsonnet -J vendor -S -e 'std.manifestYamlDoc((import "mixin.libsonnet").prometheusRules)' | gojsontoyaml > "${TOP}/${MANIFESTS}/${mixin}/rules.yaml" || :
+		jsonnet -J vendor -m "${TOP}/${MANIFESTS}/${mixin}/dashboards" -e '(import "mixin.libsonnet").grafanaDashboards' || :
+	)
 }
 
 parse_rules() {
@@ -42,6 +41,12 @@ parse_rules() {
 	done
 }
 
+panel() {
+	echo -e "{{< panel style=\"$1\" >}}"
+	echo -e "$2"
+	echo -e "{{< /panel >}}\n"
+}
+
 mixin_header() {
 	local name="$1"
 	local repo="$2"
@@ -57,30 +62,29 @@ title: $name
 
 $description
 
-{{< panel style="primary" title="Jsonnet source" >}}
-Mixin jsonnet code is available at [${repo#*//}]($url)
-{{< /panel >}}
-
 EOF
+panel "danger" "Jsonnet source code is available at [${repo#*//}]($url)"
 }
 
-# remove generated manifests and temporary directory
-rm -rf $MANIFESTS $TMPDIR
+
+cd "${TOP}" || exit 1
+# remove generated assets and temporary directory
+rm -rf "$MANIFESTS" "$TMPDIR"
 # remove generated site content
 find site/content/ ! -name '_index.md' -type f -exec rm -rf {} +
-mkdir -p $TMPDIR
 
-# Generate manifests
-cat mixins.yaml | gojsontoyaml -yamltojson > $TMPDIR/mixins.json
+mkdir -p "${TMPDIR}"
 
-for mixin in $(cat $TMPDIR/mixins.json | jq -r '.mixins[].name'); do
-	cd $TOP
-	repo="$(cat $TMPDIR/mixins.json | jq -r ".mixins[] | select(.name == \"$mixin\") | .source")"
-	subdir="$(cat $TMPDIR/mixins.json | jq -r ".mixins[] | select(.name == \"$mixin\") | .subdir")"
-	text="$(cat $TMPDIR/mixins.json | jq -r ".mixins[] | select(.name == \"$mixin\") | .text")"
+# Generate mixins 
+CONFIG=$(gojsontoyaml -yamltojson < mixins.yaml)
+
+for mixin in $(echo "$CONFIG" | jq -r '.mixins[].name'); do
+	repo="$(echo "$CONFIG" | jq -r ".mixins[] | select(.name == \"$mixin\") | .source")"
+	subdir="$(echo "$CONFIG" | jq -r ".mixins[] | select(.name == \"$mixin\") | .subdir")"
+	text="$(echo "$CONFIG" | jq -r ".mixins[] | select(.name == \"$mixin\") | .description")"
 	if [ "$text" == "null" ]; then text=""; fi
 	set +u
-	download_mixin $mixin $repo $subdir
+	download_mixin "$mixin" "$repo" "$subdir"
 	#set -u
 
 	mkdir -p "site/content/${mixin}"
@@ -96,30 +100,24 @@ for mixin in $(cat $TMPDIR/mixins.json | jq -r '.mixins[].name'); do
 	dir="$TOP/$MANIFESTS/$mixin"
 	# Alerts
 	if [ -s "$dir/alerts.yaml" ] && [ "$(stat -c%s "$dir/alerts.yaml")" -gt 20 ]; then
-		echo -e "## Alerts\n" >> $file
-		echo -e '{{< panel style="info" >}}' >> $file
-		echo -e "Complete list of pregenerated alerts is available [here](https://github.com/cloudalchemy/mixins/blob/master/manifests/$mixin/alerts.yaml)." >> $file
-		echo -e '{{< /panel >}}\n' >> $file
-
+		echo -e "## Alerts\n" >> "$file"
+		panel "warning" "Complete list of pregenerated alerts is available [here](https://github.com/cloudalchemy/mixins/blob/master/$MANIFESTS/$mixin/alerts.yaml)." >> "$file"
 		parse_rules "$(gojsontoyaml -yamltojson < "$dir/alerts.yaml")" "alert" >> "$file"
 	fi
 
 	# Recording Rules
 	if [ -s "$dir/rules.yaml" ] && [ "$(stat -c%s "$dir/rules.yaml")" -gt 20 ]; then
-		echo -e "## Recording rules\n" >> $file
-		echo -e '{{< panel style="info" >}}' >> $file
-		echo -e "Complete list of pregenerated recording rules is available [here](https://github.com/cloudalchemy/mixins/blob/master/manifests/$mixin/rules.yaml)." >> $file
-		echo -e '{{< /panel >}}\n' >> $file
-
+		echo -e "## Recording rules\n" >> "$file"
+		panel "warning" "Complete list of pregenerated recording rules is available [here](https://github.com/cloudalchemy/mixins/blob/master/$MANIFESTS/$mixin/rules.yaml)." >> "$file"
 		parse_rules "$(gojsontoyaml -yamltojson < "$dir/rules.yaml")" "record" >> "$file"
 	fi
 
 	# Dashboards
 	if [ "$(ls -A "$dir/dashboards")" ]; then
-		echo -e "## Dashboards\nFollowing dashboards are generated from mixins and hosted on github:\n\n" >> $file
-		for dashboard in $dir/dashboards/*.json; do
-			d=$(basename $dashboard)
-			echo "- [${d%.*}](https://github.com/cloudalchemy/mixins/blob/master/manifests/$mixin/dashboards/$d)" >> $file
+		echo -e "## Dashboards\nFollowing dashboards are generated from mixins and hosted on github:\n\n" >> "$file"
+		for dashboard in "$dir/dashboards"/*.json; do
+			d="$(basename "$dashboard")"
+			echo "- [${d%.*}](https://github.com/cloudalchemy/mixins/blob/master/$MANIFESTS/$mixin/dashboards/$d)" >> "$file"
 		done
 	fi
 done
